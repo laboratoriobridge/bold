4d244423283e7e877a6da69a7209263e7e597237
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-01-12 16:39:51 -0200

    Fix withTheme import

--- src/stories-addons/withTheme.tsx
+++ src/stories-addons/withTheme.tsx
@@ -1,8 +1,7 @@
 import { Renderable, RenderFunction } from '@storybook/react'
 import * as React from 'react'
 
-import { Theme } from '../../lib/styles/theme/Theme'
-import { defaultTheme, ThemeProvider } from '../styles/'
+import { defaultTheme, Theme, ThemeProvider } from '../styles/'
 
 export const withTheme = (theme: Theme = defaultTheme) =>
     (story: RenderFunction, context: { kind: string, story: string }): Renderable => {
