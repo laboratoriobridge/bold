be950a3d05df1ce4bfad3610e1a744a1a75fcc68
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-02-06 13:53:06 -0200

    Parse dos MaskedFields e inclusão de placeholders

--- src/components/form/field/MaskedField/masks.tsx
+++ src/components/form/field/MaskedField/masks.tsx
@@ -8,13 +8,13 @@ import { MaskedField, MaskedFieldProps } from './MaskedField'
 export interface GenericMaskedProps extends Omit<MaskedFieldProps, 'mask'> {
 }
 
-const createMaskedField = (displayName: string, mask: MaskType) => {
+const createMaskedField = (displayName: string, mask: MaskType, props?: Partial<GenericMaskedProps>) => {
     return class extends React.PureComponent<GenericMaskedProps> {
         static displayName = displayName
 
         render() {
             return (
-                <MaskedField mask={mask} {...this.props} />
+                <MaskedField mask={mask} {...props} {...this.props} />
             )
         }
     }
@@ -23,7 +23,20 @@ const createMaskedField = (displayName: string, mask: MaskType) => {
 export const masks: { [key: string]: MaskType } = {
     telefone: ['(', /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/],
     cpf: [/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/],
+    cep: [/\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/],
 }
 
-export const TelefoneField = createMaskedField('TelefoneField', masks.telefone)
-export const CpfField = createMaskedField('CpfField', masks.cpf)
+const onlyNumbers = (value: string) => value.replace(/[^\d]/g, '')
+
+export const TelefoneField = createMaskedField('TelefoneField', masks.telefone, {
+    parse: onlyNumbers,
+    placeholder: '(__) _____-____',
+})
+export const CpfField = createMaskedField('CpfField', masks.cpf, {
+    parse: onlyNumbers,
+    placeholder: '___.___.___-__',
+})
+export const CepField = createMaskedField('CepField', masks.cep, {
+    parse: onlyNumbers,
+    placeholder: '_____-___',
+})
