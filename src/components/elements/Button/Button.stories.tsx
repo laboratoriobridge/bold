a6b132ec0ac2e96e4470e1b521789d54133a66d9
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-02-07 09:45:03 -0200

    Animação de 'loading' no Button

--- src/components/elements/Button/Button.stories.tsx
+++ src/components/elements/Button/Button.stories.tsx
@@ -1,10 +1,11 @@
+import { boolean, select, text, withKnobs } from '@storybook/addon-knobs'
+import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 
-import { storiesOf } from '@storybook/react'
-import { withKnobs, text, boolean, select } from '@storybook/addon-knobs'
-import { Button, Type } from './Button'
-import { withTheme } from '../../../stories-addons/withTheme'
 import { withPropTypes } from '../../../stories-addons/withPropTypes'
+import { withTheme } from '../../../stories-addons/withTheme'
+
+import { Button, Type } from './Button'
 
 const typeOptions: {[key in Type]: string} = {
     'normal': 'normal',
@@ -18,11 +19,15 @@ const typeOptions: {[key in Type]: string} = {
 
 storiesOf('Components', module)
     .addDecorator(withPropTypes(`
-    Botões expressam que alguma atividade vai acontecer quando o usuário clicar nele. São usados para iniciar uma ação, alguns exemplos incluem a ação de Adicionar, Salvar, Deletar.
+    Botões expressam que alguma atividade vai acontecer quando o usuário clicar nele.
+    São usados para iniciar uma ação, alguns exemplos incluem a ação de Adicionar, Salvar, Deletar.
 
     ### BOAS PRÁTICAS:
-    - Não use botões como elementos de navegação. Use Links ao invés,  porque levam o usuário a uma nova página que não está associada com a ação.
-    - A label do botão indica ao usuário o que vai acontecer quando ele clicar no botão. Use somente verbos que descrevem a ação, como Adicionar ou Deletar. As labels devem seguir o padrão Capitalized (apenas a primeira letra na palavra em maiúsculo).
+    - Não use botões como elementos de navegação. Use Links ao invés,  porque levam o usuário a uma
+    nova página que não está associada com a ação.
+    - A label do botão indica ao usuário o que vai acontecer quando ele clicar no botão.
+    Use somente verbos que descrevem a ação, como Adicionar ou Deletar. As labels devem seguir o
+    padrão Capitalized (apenas a primeira letra na palavra em maiúsculo).
     `))
     .addDecorator(withKnobs)
     .addDecorator(withTheme())
@@ -31,5 +36,6 @@ storiesOf('Components', module)
             disabled={boolean('disabled', false)}
             type={select('type', typeOptions, 'normal')}
             label={text('label', 'BUTTON')}
+            loading={boolean('loading', false)}
         />
     ))
