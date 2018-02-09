bd9d6754d191825382f718759f08705aad396200
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-02-09 09:26:27 -0200

    Add line-height on InfoLabel

--- src/components/elements/InfoLabel/InfoLabel.tsx
+++ src/components/elements/InfoLabel/InfoLabel.tsx
@@ -13,12 +13,15 @@ export class InfoLabel extends React.Component<InfoLabelProps> {
     render() {
         const { css, title, children, titleStyles, childStyles } = this.props
         const styles = {
+            infoLabel: {
+                lineHeight: 1.5,
+            },
             title: {
                 fontWeight: 'bold',
             },
         }
         return (
-            <div className={css()}>
+            <div className={css(styles.infoLabel)}>
                 <div className={css(styles.title, titleStyles)}>{title}</div>
                 <div className={css(childStyles)}>{children}</div>
             </div>
