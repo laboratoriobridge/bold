a0d99cb5925453a6da00ebdd66ae262533259d8b
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-04-10 10:40:18 -0300

    Add justifyContent and alignItems props for Flow

--- src/components/layout/Flow/Flow.stories.tsx
+++ src/components/layout/Flow/Flow.stories.tsx
@@ -4,6 +4,7 @@ import * as React from 'react'
 
 import { withPropTypes, withTheme } from '../../../stories-addons'
 import { Button } from '../../elements/button/Button/Button'
+import { AlignItems, JustifyContent } from '../../grid/Grid/Grid'
 
 import { Flow } from './Flow'
 import { HFlow } from './HFlow'
@@ -12,6 +13,23 @@ import { VFlow } from './VFlow'
 const directionOptions = {
     'horizontal': 'horizontal',
     'vertical': 'vertical',
+}
+
+const justifyContentOptions: { [key in JustifyContent]: string } = {
+    'flex-start': 'flex-start',
+    'flex-end': 'flex-end',
+    'center': 'center',
+    'space-between': 'space-between',
+    'space-around': 'space-around',
+    'space-evenly': 'space-evenly',
+}
+
+const alignItemsOptions: { [key in AlignItems]: string } = {
+    'flex-start': 'flex-start',
+    'flex-end': 'flex-end',
+    'center': 'center',
+    'baseline': 'baseline',
+    'stretch': 'stretch',
 }
 
 storiesOf('Layout', module)
@@ -24,6 +42,8 @@ storiesOf('Layout', module)
                 hSpacing={number('hSpacing', 1)}
                 vSpacing={number('vSpacing', 0)}
                 direction={select('direction', directionOptions, 'horizontal')}
+                alignItems={select('alignItems', alignItemsOptions, null)}
+                justifyContent={select('justifyContent', justifyContentOptions, null)}
             >
                 <span>1</span>
                 <span>2</span>
@@ -35,6 +55,8 @@ storiesOf('Layout', module)
                 hSpacing={number('hSpacing', 1)}
                 vSpacing={number('vSpacing', 0)}
                 direction={select('direction', directionOptions, 'horizontal')}
+                alignItems={select('alignItems', alignItemsOptions, null)}
+                justifyContent={select('justifyContent', justifyContentOptions, null)}
             >
                 <Button label='Botão 1' type='primary' />
                 <Button label='Botão 2' />
