73641cc6a1d5b202fa5819c63469b9128478c784
Mateus Alfredo Batista <mateus.batista@bridge.ufsc.br>
2018-05-08 11:43:41 -0300

    ampliado testes

--- src/components/elements/Dropdown/IconDropdown.test.tsx
+++ src/components/elements/Dropdown/IconDropdown.test.tsx
@@ -3,22 +3,40 @@ import * as React from 'react'
 
 import { withTheme } from '../../../test'
 
+import { DropdownItem } from './DropdownMenu'
 import { IconDropdown } from './IconDropdown'
 
 const options = [
     {
         title: 'Option 1',
-        onClick: () => alert('Option 1 selected'),
+        onClick: jest.fn(),
     },
     {
         title: 'Option 2',
-        onClick: () => alert('Option 2 selected'),
+        onClick: jest.fn(),
     },
 ]
 
 describe('IconDropdown', () => {
-    it('deve renderizar corretamente', () => {
+    it('should render correctly', () => {
         const wrapper = render(withTheme(<IconDropdown options={options} icon={'triangleDown'} />))
         expect(wrapper).toMatchSnapshot()
     })
+
+    it('should expand onClick', () => {
+
+        const wrapper = mount(withTheme(<IconDropdown options={options} icon={'triangleDown'} />))
+        expect(wrapper.find('li').length).toEqual(0)
+        wrapper.find('a').simulate('click')
+        expect(wrapper.find('li').length).toEqual(2)
+
+    })
+
+    it('should call function on DropdownItem click', () => {
+        const wrapper = mount(withTheme(<IconDropdown options={options} icon={'triangleDown'} />))
+        wrapper.find('a').simulate('click')
+        wrapper.find(DropdownItem).first().find('a').simulate('click')
+        expect(options[0].onClick).toHaveBeenCalled()
+    })
+
 })
