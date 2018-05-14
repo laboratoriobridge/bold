137817e378faf670a9c4d73c3f1b04ee2caad69f
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-05-14 17:54:56 -0300

    Fix table exports

--- src/components/elements/table/index.ts
+++ src/components/elements/table/index.ts
@@ -1,5 +1,12 @@
 export * from './Table'
 
-export { DataTable, DataTableProps } from './DataTable/DataTable'
-export { PagedTable, PagedTableProps } from './PagedTable/PagedTable'
+export { SortableLabel, SortableLabelProps } from './SortableLabel/SortableLabel'
 export { TableFooter, TableFooterProps } from './TableFooter/TableFooter'
+
+export { DataTable, DataTableProps } from './DataTable/DataTable'
+export { TableFilledBody, TableFilledBodyProps } from './DataTable/TableFilledBody'
+export { TableLoadingRow, TableLoadingRowProps } from './DataTable/TableLoadingRow'
+export { TablePlaceholderRow, TablePlaceholderRowProps } from './DataTable/TablePlaceholderRow'
+
+export { PagedTable, PagedTableProps } from './PagedTable/PagedTable'
+export { PagedTableConnected, PagedTableConnectedProps } from './PagedTable/PagedTableConnected'
