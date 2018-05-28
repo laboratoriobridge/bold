41d66bd1be89f8c0c4314037f2d976552b6a7638
mateus-batista <mateusalfredobatista@gmail.com>
2018-05-28 15:01:47 -0300

    New arrow left icon (#126)
    * New arrow left icon
    
    * New arrow left icon

---
+++ src/components/elements/Icon/generated/ArrowLeft.tsx
@@ -0,0 +1,12 @@
+/* tslint:disable */
+import * as React from "react"
+import { GeneratedIconProps } from "../GeneratedIconProps"
+
+export const ArrowLeft = (props: GeneratedIconProps) => (
+  <svg viewBox="0 0 24 24" width="1em" height="1em" {...props}>
+    <path
+      d="M18 11H9.94l3.95-3.95-1.42-1.41L6.11 12l6.36 6.36 1.42-1.41L9.94 13H18v-2z"
+      id="arrow-left"
+    />
+  </svg>
+)
