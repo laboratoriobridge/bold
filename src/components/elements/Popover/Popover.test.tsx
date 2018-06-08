9606b788cf34d91c92b417387698fcdbea3aa2ca
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-06-08 13:48:28 -0300

    Popover tests

---
+++ src/components/elements/Popover/Popover.test.tsx
@@ -0,0 +1,20 @@
+import { render } from 'enzyme'
+import * as React from 'react'
+
+import { withTheme } from '../../../test'
+
+import { Popover } from './Popover'
+
+it('should render correctly', () => {
+    expect(render(withTheme(
+        <Popover title='Title' text='Popover text' placement='bottom-start'>
+            <span>Testing with title</span>
+        </Popover>
+    ))).toMatchSnapshot()
+
+    expect(render(withTheme(
+        <Popover text='Popover text'>
+            <span>Testing no title</span>
+        </Popover>
+    ))).toMatchSnapshot()
+})
