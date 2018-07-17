e89aad8c08a3e2a884c70bfbf11997cd405ff237
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-07-17 16:41:02 -0300

    New icons

---
+++ src/components/elements/Icon/generated/Lock.tsx
@@ -0,0 +1,11 @@
+/* tslint:disable */
+import * as React from "react";
+import { GeneratedIconProps } from "../GeneratedIconProps";
+
+export const Lock = (props: GeneratedIconProps) => (
+  <svg viewBox="0 0 24 24" width="1em" height="1em" {...props}>
+    <rect x={4} y={10} width={16} height={12.04} rx={2} ry={2} />
+    <path d="M13 2h-2a5 5 0 0 0-5 5v5a1 1 0 0 0 1 1 1 1 0 0 0 1-1V7a3 3 0 0 1 3-3h2a3 3 0 0 1 3 3v5a1 1 0 0 0 1 1 1 1 0 0 0 1-1V7a5 5 0 0 0-5-5z" />
+    <rect x={11.17} y={14.04} width={2} height={5} rx={1} ry={1} fill="#fff" />
+  </svg>
+);
