aa7c55c6e83cec0ef3f473e235627dee7e323cb6
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-08-23 13:50:06 -0300

    Remove specific behaviours from Select components

--- src/components/form/input/AsyncSelect/AsyncSelect.tsx
+++ src/components/form/input/AsyncSelect/AsyncSelect.tsx
@@ -1,26 +1,13 @@
 import * as React from 'react'
 import ReactAsyncSelect, { Props as ReactAsyncSelectProps } from 'react-select/lib/Async'
-import { OptionsType } from 'react-select/lib/types'
 
 import { withStyles, WithStylesProps } from '../../../../styles/index'
-import { Omit } from '../../../../util/types'
 import { createSelectStyles } from '../Select/createSelectStyle'
 import { DefaultOptionType } from '../Select/Select'
 
-export type OmitProps = 'loadOptions' | 'defaultOptions' | 'cacheOptions'
-
-export interface AsyncSelectRequestParams {
-    query: string
-    pageSize: number
-}
-
 export interface AsyncSelectProps<OptionType = DefaultOptionType> extends WithStylesProps,
-    Omit<ReactAsyncSelectProps<OptionType>, OmitProps> {
-    getPage: (params: AsyncSelectRequestParams) => Promise<any>
-    pageSize?: number
-    searchDelay?: number
+    ReactAsyncSelectProps<OptionType> {
     status?: '' | 'error'
-    value?: any
     disabled?: boolean
 }
 
@@ -29,7 +16,6 @@ export class AsyncSelect<OptionType = DefaultOptionType> extends React.Component
 
     static defaultProps: Partial<AsyncSelectProps<any>> = {
         pageSize: 10,
-        searchDelay: 500,
         backspaceRemovesValue: false,
         isMulti: false,
         isClearable: true,
@@ -40,8 +26,6 @@ export class AsyncSelect<OptionType = DefaultOptionType> extends React.Component
         getOptionValue: (option) => option && option.value,
     }
 
-    private typingTimer: number
-
     render() {
         const { css, theme, status, disabled, ...rest } = this.props
 
@@ -51,30 +35,10 @@ export class AsyncSelect<OptionType = DefaultOptionType> extends React.Component
             <ReactAsyncSelect
                 classNamePrefix='react-select-async'
                 styles={styles}
-                loadOptions={this.loadOptions}
                 isDisabled={disabled}
                 closeMenuOnSelect={!this.props.isMulti}
-                cacheOptions={false}
-                defaultOptions={[]}
                 {...rest}
             />
         )
-    }
-
-    private loadOptions = (inputValue: string, callback: (options: OptionsType<OptionType>) => void) => {
-        if (this.typingTimer) {
-            clearTimeout(this.typingTimer)
-        }
-
-        this.typingTimer = window.setTimeout(() => this.getPage(inputValue, callback), this.props.searchDelay)
-    }
-
-    private getPage = (inputValue: string, callback: (options: OptionsType<OptionType>) => void) => {
-        this.props.getPage({
-            query: inputValue,
-            pageSize: this.props.pageSize,
-        }).then(result => {
-            callback(result.data || result)
-        })
     }
 }
