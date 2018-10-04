a103ea79e5c8eaf4ca6d0d64fa8582c557815f9a
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-10-04 16:37:08 -0300

    Remove deprecation warnings from storybook and adding default addons globally

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,15 @@
-import { setDefaults } from '@storybook/addon-info'
-import { setOptions } from '@storybook/addon-options'
+import { withInfo } from '@storybook/addon-info'
+import { withKnobs } from '@storybook/addon-knobs'
+import { withOptions } from '@storybook/addon-options'
 import { addDecorator, configure } from '@storybook/react'
 
 import { withTheme } from '../src/stories-addons'
 
-setOptions({
+addDecorator(withOptions({
   name: 'Bridge React',
   url: 'https://github.com/laboratoriobridge/bridge-react',
-})
-
-setDefaults({
+}))
+addDecorator(withInfo({
   inline: true,
   styles: {
     infoBody: {
@@ -26,9 +26,9 @@ setDefaults({
       padding: '0 45px 0 45px',
     },
   },
-})
-
+}))
 addDecorator(withTheme())
+addDecorator(withKnobs())
 
 const req = require.context('../src', true, /.stories.tsx?$/)
 
