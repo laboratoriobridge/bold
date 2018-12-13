06ff166076e9a374763534a04acb7754a533ad62
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-12-13 09:46:40 -0200

    Using withField hoc for MaskedField

--- src/components/form/field/MaskedField/MaskedField.tsx
+++ src/components/form/field/MaskedField/MaskedField.tsx
@@ -1,34 +1,6 @@
-import * as React from 'react'
-
-import { FieldWrapperProps } from '../../../form/FieldWrapper'
-import { Field, FieldProps, RenderProps } from '../../finalForm/Field'
+import { BaseFieldProps, withField } from '../../finalForm/Field'
 import { MaskedInput, MaskedInputProps } from '../../input/MaskedInput/MaskedInput'
 
-export interface MaskedFieldProps extends FieldWrapperProps, MaskedInputProps,
-    Pick<FieldProps, 'parse' | 'format'> {
-    name: string
-}
+export type MaskedFieldProps = BaseFieldProps<MaskedInputProps>
 
-export class MaskedField extends React.Component<MaskedFieldProps> {
-
-    render() {
-        return (
-            <Field
-                {...this.props}
-                render={this.renderInput}
-            />
-        )
-    }
-
-    private renderInput = (props: RenderProps) => {
-        const { format, parse, ...rest } = this.props
-        return (
-            <MaskedInput
-                {...rest}
-                {...props.input}
-                status={props.hasError ? 'error' : undefined}
-            />
-        )
-    }
-
-}
+export const MaskedField = withField(MaskedInput)
