3c516425edf19bc96e3b93eb41e5fe057c2c9b9a
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-12-18 10:25:28 -0200

    Create FocusManagerContainer component

---
+++ src/components/elements/FocusManagerContainer/FocusManagerContainer.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react'
+
+export interface FocusManagerContainerProps {
+    onFocusIn?(e: React.FocusEvent<HTMLDivElement>): void
+    onFocusOut?(e: React.FocusEvent<HTMLDivElement>): void
+}
+
+export interface FocusManagerContainerState {
+    isManagingFocus: boolean
+}
+
+/**
+ * Fires events when focus enters or leaves the container.
+ */
+export class FocusManagerContainer extends React.Component<FocusManagerContainerProps, FocusManagerContainerState> {
+
+    state: FocusManagerContainerState = {
+        isManagingFocus: false,
+    }
+
+    private timeoutId: number
+
+    render() {
+        return (
+            <div onBlur={this.handleBlur} onFocus={this.handleFocus}>{this.props.children}</div>
+        )
+    }
+
+    private handleBlur = (e: React.FocusEvent<HTMLDivElement>) => {
+        this.timeoutId = window.setTimeout(() => {
+            if (this.state.isManagingFocus) {
+                this.setState({ isManagingFocus: false }, () => {
+                    if (this.props.onFocusOut) {
+                        this.props.onFocusOut(e)
+                    }
+                })
+            }
+        }, 0)
+    }
+
+    private handleFocus = (e: React.FocusEvent<HTMLDivElement>) => {
+        window.clearTimeout(this.timeoutId)
+        if (!this.state.isManagingFocus) {
+            this.setState({ isManagingFocus: true }, () => {
+                if (this.props.onFocusIn) {
+                    this.props.onFocusIn(e)
+                }
+            })
+        }
+    }
+}
---
+++ src/components/elements/FocusManagerContainer/index.ts
@@ -0,0 +1,1 @@
+export { FocusManagerContainer, FocusManagerContainerProps } from './FocusManagerContainer'
