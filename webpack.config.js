5caa541eeaeabfc9130f6b7b6211c1df1ec2a5fb
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-02-20 11:53:49 -0300

    Webpack exporting sourcemap to production build

--- webpack.config.js
+++ webpack.config.js
@@ -3,59 +3,58 @@ const { CheckerPlugin } = require('awesome-typescript-loader')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'development',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'lib'),
-        library: 'bridge-react',
-        libraryTarget: 'umd',
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                loader: 'awesome-typescript-loader',
-                test: /\.tsx?$/,
-            },
-            {
-                loader: 'raw-loader',
-                test: /\.css$/,
-            },
-        ],
-    },
-    plugins: [
-        new CheckerPlugin(),
+  entry: './src/index.ts',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'lib'),
+    library: 'bridge-react',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        loader: 'awesome-typescript-loader',
+        test: /\.tsx?$/,
+      },
+      {
+        loader: 'raw-loader',
+        test: /\.css$/,
+      },
     ],
-    externals: [
-        'emotion',
-        'final-form',
-        'history',
-        'moment',
-        'react',
-        'react-dom',
-        'react-final-form',
-        'react-router',
-        'react-router-dom',
-        'tslint-react',
+  },
+  plugins: [new CheckerPlugin()],
+  externals: [
+    'emotion',
+    'final-form',
+    'history',
+    'moment',
+    'react',
+    'react-dom',
+    'react-final-form',
+    'react-router',
+    'react-router-dom',
+    'tslint-react',
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        uglifyOptions: {
+          // Keep function original names to preserve React component display names
+          compress: {
+            warnings: false,
+            keep_fnames: true,
+          },
+          mangle: {
+            keep_fnames: true,
+          },
+        },
+      }),
     ],
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                uglifyOptions: {
-                    // Keep function original names to preserve React component display names
-                    compress: {
-                        warnings: false,
-                        keep_fnames: true,
-                    },
-                    mangle: {
-                        keep_fnames: true,
-                    },
-                },
-            }),
-        ],
-    },
+  },
 }
