16881e73cec1ee3bf30d4fd8664b3dd349c11b59
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-02-25 09:12:32 -0300

    Add deprecated warning to 'withStyles' doc

--- src/styles/withStyles.tsx
+++ src/styles/withStyles.tsx
@@ -4,37 +4,31 @@ import React from 'react'
 import { Theme } from './theme/createTheme'
 import { ThemeContext } from './theme/ThemeContext'
 
+/**
+ * @deprecated Use the `useStyles` hook instead.
+ */
 export interface WithStylesProps {
-    theme?: Theme
-    css?: (...styles: Interpolation[]) => string
+  theme?: Theme
+  css?: (...styles: Interpolation[]) => string
 }
 
 export interface Styles {
-    [key: string]: Interpolation
+  [key: string]: Interpolation
 }
 
-export function withStyles<P extends WithStylesProps, T extends React.ComponentType<P>>(
-    WrappedComponent: T
-): T {
-    const Cmp = WrappedComponent as React.ComponentType<P>
+/**
+ * @deprecated Use the `useStyles` hook instead.
+ */
+export function withStyles<P extends WithStylesProps, T extends React.ComponentType<P>>(WrappedComponent: T): T {
+  const Cmp = WrappedComponent as React.ComponentType<P>
 
-    class WithStyles extends React.Component<P> {
-        static displayName = `${WrappedComponent.displayName || WrappedComponent.name}`
+  class WithStyles extends React.Component<P> {
+    static displayName = `${WrappedComponent.displayName || WrappedComponent.name}`
 
-        render() {
-            return (
-                <ThemeContext.Consumer>
-                    {(theme) => (
-                        <Cmp
-                            {...this.props}
-                            css={css}
-                            theme={theme}
-                        />
-                    )}
-                </ThemeContext.Consumer>
-            )
-        }
+    render() {
+      return <ThemeContext.Consumer>{theme => <Cmp {...this.props} css={css} theme={theme} />}</ThemeContext.Consumer>
     }
+  }
 
-    return WithStyles as T
+  return WithStyles as T
 }
