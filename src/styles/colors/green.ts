ff6852d448935b465750864c778318817e4da907
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-03-01 09:02:22 -0300

    Add invertColorScale util function

--- src/styles/colors/green.ts
+++ src/styles/colors/green.ts
@@ -1,14 +1,14 @@
 import { ColorScale } from '.'
 
 export const green: ColorScale = {
-    c10: '#0C1F10',
-    c20: '#09421C',
-    c30: '#085E17',
-    c40: '#097D05',
-    c50: '#0D8F20',
-    c60: '#28AB16',
-    c70: '#7ED945',
-    c80: '#B1F081',
-    c90: '#E3FAD2',
-    c100: '#FFFFFF',
+  c10: '#0C1F10',
+  c20: '#09421C',
+  c30: '#085E17',
+  c40: '#097D05',
+  c50: '#0D8F20',
+  c60: '#28AB16',
+  c70: '#7ED945',
+  c80: '#B1F081',
+  c90: '#E3FAD2',
+  c100: '#FFFFFF',
 }
--- src/styles/colors/red.ts
+++ src/styles/colors/red.ts
@@ -1,14 +1,14 @@
 import { ColorScale } from '.'
 
 export const red: ColorScale = {
-    c10: '#3D110E',
-    c20: '#75211B',
-    c30: '#AD2E24',
-    c40: '#D83A2E',
-    c50: '#E64949',
-    c60: '#ED6868',
-    c70: '#FCA7A7',
-    c80: '#FCD2D2',
-    c90: '#FCF0F0',
-    c100: '#FFFFFF',
+  c10: '#3D110E',
+  c20: '#75211B',
+  c30: '#AD2E24',
+  c40: '#D83A2E',
+  c50: '#E64949',
+  c60: '#ED6868',
+  c70: '#FCA7A7',
+  c80: '#FCD2D2',
+  c90: '#FCF0F0',
+  c100: '#FFFFFF',
 }
