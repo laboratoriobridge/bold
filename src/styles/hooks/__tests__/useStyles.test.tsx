db108f37601ff5cbf149a0d0465ce1f9f584e497
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-03-01 10:15:19 -0300

    Style hooks improvements and add tests

---
+++ src/styles/hooks/__tests__/useStyles.test.tsx
@@ -0,0 +1,61 @@
+import { css as emotionCss } from 'emotion'
+import React from 'react'
+import { renderHook } from 'react-hooks-testing-library'
+
+import { createTheme } from '../../theme/createTheme'
+import { ThemeContext } from '../../theme/ThemeContext'
+import { useStyles } from '../useStyles'
+
+jest.unmock('../useTheme')
+
+it('should return a map of classNames generated from a map of css properties', () => {
+  const factory = () => ({
+    class1: {
+      color: 'red',
+    },
+    class2: {
+      color: 'blue',
+    },
+  })
+
+  const styles = factory()
+
+  const { result } = renderHook(() => useStyles(factory))
+  expect(result.current.classes).toEqual({
+    class1: emotionCss(styles.class1),
+    class2: emotionCss(styles.class2),
+  })
+})
+
+it('should call the factory with additional arguments passed to the function', () => {
+  const factory = jest.fn(() => ({}))
+  const theme = createTheme()
+
+  renderHook(() => useStyles(factory, 'foo', 42, { obj: true }), {
+    wrapper: ({ children }) => <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>,
+  })
+  expect(factory).toHaveBeenLastCalledWith(theme, 'foo', 42, { obj: true })
+})
+
+it('should call the factory with current theme from ThemeContext', () => {
+  const factory = jest.fn(() => ({}))
+  const theme = createTheme()
+
+  renderHook(() => useStyles(factory), {
+    wrapper: ({ children }) => <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>,
+  })
+  expect(factory).toHaveBeenLastCalledWith(theme)
+})
+
+it('should return the current theme from ThemeContext', () => {
+  const theme = createTheme()
+  const { result } = renderHook(() => useStyles(), {
+    wrapper: ({ children }) => <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>,
+  })
+  expect(result.current.theme).toEqual(theme)
+})
+
+it('should return the "css" function', () => {
+  const { result } = renderHook(() => useStyles())
+  expect(result.current.css).toEqual(emotionCss)
+})
