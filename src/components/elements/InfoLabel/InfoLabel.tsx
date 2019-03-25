d56b73a68b18c59003ffd4fea3dcab28d071647c
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-03-25 17:00:41 -0300

    Refactor InfoLabel component

--- src/components/elements/InfoLabel/InfoLabel.tsx
+++ src/components/elements/InfoLabel/InfoLabel.tsx
@@ -1,39 +1,39 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 
-import { Styles, withStyles, WithStylesProps } from '../../../styles'
+import { Theme, useStyles } from '../../../styles'
 
-export interface InfoLabelProps extends WithStylesProps {
-    title: string
-    titleStyles?: any
-    childStyles?: any
-    placeholder?: string
+export interface InfoLabelProps {
+  title: string
+  titleStyles?: any
+  childStyles?: any
+  placeholder?: string
+  children?: React.ReactNode
 }
 
-@withStyles
-export class InfoLabel extends React.PureComponent<InfoLabelProps> {
+export function InfoLabel(props: InfoLabelProps) {
+  const { title, children, titleStyles, childStyles } = props
+  const { classes, css } = useStyles(createStyles, props)
 
-    render() {
-        const { css, theme, title, children, titleStyles, childStyles, placeholder } = this.props
-        const styles: Styles = {
-            infoLabel: {
-                lineHeight: 1.5,
-            },
-            title: {
-                fontWeight: 'bold',
-            },
-            child: {
-                ':empty::before': {
-                    content: `"${placeholder || '-'}"`,
-                    color: theme.pallete.text.secondary,
-                    fontStyle: 'italic',
-                },
-            },
-        }
-        return (
-            <div className={css(styles.infoLabel)}>
-                <div className={css(styles.title, titleStyles)}>{title}</div>
-                <div className={css(styles.child, childStyles)}>{children}</div>
-            </div>
-        )
-    }
+  return (
+    <div className={classes.infoLabel}>
+      <div className={css(classes.title, titleStyles)}>{title}</div>
+      <div className={css(classes.child, childStyles)}>{children}</div>
+    </div>
+  )
 }
+
+export const createStyles = (theme: Theme, { placeholder }: InfoLabelProps) => ({
+  infoLabel: {
+    lineHeight: 1.5,
+  } as CSSProperties,
+  title: {
+    fontWeight: 'bold',
+  } as CSSProperties,
+  child: {
+    ':empty::before': {
+      content: `"${placeholder || '-'}"`,
+      color: theme.pallete.text.secondary,
+      fontStyle: 'italic',
+    },
+  } as CSSProperties,
+})
