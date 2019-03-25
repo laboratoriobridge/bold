de116de3f1ccc378d20f30437fe0c0a1c2f9fb8b
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-03-25 17:07:41 -0300

    Refactor Paper component

--- src/components/elements/Paper/Paper.test.tsx
+++ src/components/elements/Paper/Paper.test.tsx
@@ -1,14 +1,23 @@
-import { render } from 'enzyme'
 import React from 'react'
-
-import { withTheme } from '../../../test'
+import { render } from 'react-testing-library'
 
 import { Paper } from './Paper'
 
 it('should render correctly', () => {
-    expect(render(withTheme(
-        <Paper elevation={20}>
-            <span>Testing with title</span>
-        </Paper>
-    ))).toMatchSnapshot()
+  const { container } = render(
+    <Paper elevation={20}>
+      <span>Testing with title</span>
+    </Paper>
+  )
+  expect(container).toMatchSnapshot()
+})
+
+it('should accept style prop', () => {
+  const { container } = render(<Paper style={{ color: 'red' }} />)
+  expect(container).toMatchSnapshot()
+})
+
+it('should pass down props to the underlying HTML div element', () => {
+  const { container } = render(<Paper id='paper-1' />)
+  expect(container.querySelector('div').getAttribute('id')).toEqual('paper-1')
 })
