40b4867b4aa80b8d9fab3a4cafa4fda8c9f89475
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-03-26 12:17:31 -0300

    Refactor AriaControlNav

--- src/components/elements/AriaControlNav/AriaControlNav.tsx
+++ src/components/elements/AriaControlNav/AriaControlNav.tsx
@@ -1,39 +1,27 @@
 import React from 'react'
 
-import { withStyles, WithStylesProps } from '../../../styles'
 import { Button } from '../Button'
 import { HFlow } from '../Flow/HFlow'
 import { Icon } from '../Icon'
 
-export interface AriaControlNavProps extends WithStylesProps {}
+export interface AriaControlNavProps {}
 
-@withStyles
-export class AriaControlNav extends React.Component<AriaControlNavProps> {
-  render() {
-    return (
-      <HFlow hSpacing={0.5}>
-        <Button size='small' skin='ghost' onClick={this.handleDecreaseFont}>
-          <Icon icon='decreaseFont' />
-        </Button>
-        <Button size='small' skin='ghost' onClick={this.handleIncreaseFont}>
-          <Icon icon='increaseFont' />
-        </Button>
-        <Button size='small' skin='ghost' onClick={this.handleContrastChange}>
-          <Icon icon='contrast' />
-        </Button>
-      </HFlow>
-    )
-  }
+export function AriaControlNav(props: AriaControlNavProps) {
+  const handleDecreaseFont = () => null
+  const handleIncreaseFont = () => null
+  const handleContrastChange = () => null
 
-  private handleDecreaseFont = () => {
-    return null
-  }
-
-  private handleIncreaseFont = () => {
-    return null
-  }
-
-  private handleContrastChange = () => {
-    return null
-  }
+  return (
+    <HFlow hSpacing={0.5}>
+      <Button size='small' skin='ghost' onClick={handleDecreaseFont}>
+        <Icon icon='decreaseFont' />
+      </Button>
+      <Button size='small' skin='ghost' onClick={handleIncreaseFont}>
+        <Icon icon='increaseFont' />
+      </Button>
+      <Button size='small' skin='ghost' onClick={handleContrastChange}>
+        <Icon icon='contrast' />
+      </Button>
+    </HFlow>
+  )
 }
