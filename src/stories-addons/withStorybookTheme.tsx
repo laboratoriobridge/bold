265317fca300c2ac71fc7c733f2909df22c99e27
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-04-01 16:44:34 -0300

    Remove surface background color from global body styles

--- src/stories-addons/withStorybookTheme.tsx
+++ src/stories-addons/withStorybookTheme.tsx
@@ -2,17 +2,9 @@ import { StoryContext } from '@storybook/addon-knobs'
 import { Renderable, RenderFunction } from '@storybook/react'
 import React from 'react'
 
-import { createTheme, Theme, ThemeProvider } from '../styles/'
-import { merge } from '../util'
+import { createTheme, ThemeProvider } from '../styles/'
 
-const storybookTheme = merge({}, createTheme(), {
-  pallete: {
-    surface: {
-      background: '#fff',
-      main: '#fff',
-    },
-  },
-} as Partial<Theme>)
+const storybookTheme = createTheme()
 
 export const withStorybookTheme = (story: RenderFunction, context: StoryContext): Renderable => {
   return <ThemeProvider theme={storybookTheme}>{story()}</ThemeProvider>
