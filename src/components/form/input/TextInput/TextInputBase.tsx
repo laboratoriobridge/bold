36c4149fc1160263e9d9aed760ef57fa720b6dd6
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-04-10 15:45:36 -0300

    Remove input type search webkit decorations

--- src/components/form/input/TextInput/TextInputBase.tsx
+++ src/components/form/input/TextInput/TextInputBase.tsx
@@ -41,6 +41,12 @@ export const createStyleParts = (theme: Theme) => ({
     transitionProperty: 'box-shadow',
     transitionDuration: '.2s',
 
+    // Remove input type="search" decorations
+    '&[type="search"]::-webkit-search-decoration': { display: 'none' },
+    '&[type="search"]::-webkit-search-cancel-button': { display: 'none' },
+    '&[type="search"]::-webkit-search-results-button': { display: 'none' },
+    '&[type="search"]::-webkit-search-results-decoration': { display: 'none' },
+
     '&:required': {
       boxShadow: 'none',
     },
