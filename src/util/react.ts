db1d575e8cf25c763bbe7e7b89f81977237586fd
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-04-11 16:46:52 -0300

    Add react util functions, tests and doc

--- src/util/react.ts
+++ src/util/react.ts
@@ -1,49 +1,35 @@
 import { MutableRefObject, Ref } from 'react'
 
+/**
+ * Compose multiple react refs.
+ *
+ * @returns A new ref that passes down the Element to all ref arguments.
+ */
 export function composeRefs<T>(...refs: Array<Ref<T>>) {
-    return (value: T) => refs.forEach(ref => setRef(ref, value))
+  return (value: T) => refs.forEach(ref => setRef(ref, value))
 }
 
+/**
+ * Changes the value of a ref.
+ * The ref can be a function ref or a object ref.
+ *
+ * @param ref The ref to have its value changed.
+ * @param value The new value.
+ */
 export function setRef<T>(ref: Ref<T>, value: any) {
-    if (typeof ref === 'function') {
-        ref(value)
-    } else if (ref) {
-        (ref as MutableRefObject<T>).current = value
-    }
+  if (typeof ref === 'function') {
+    ref(value)
+  } else if (ref) {
+    const mutableRef = ref as MutableRefObject<T>
+    mutableRef.current = value
+  }
 }
 
 /**
- * Returns the next sibling of `element` that matches the `predicate`.
- * Returns `null` if not found.
+ * Compose multiple event handlers.
+ *
+ * @returns A new function that triggers all handlers passed as parameters, in order.
  */
-export function getNextSibling(element: Element, predicate: (sibling: Element) => boolean) {
-    let current: Element = element
-
-    while (current.nextElementSibling) {
-        current = current.nextElementSibling
-
-        if (predicate(current)) {
-            return current
-        }
-    }
-
-    return null
-}
-
-/**
- * Returns the previous sibling of `element` that matches the `predicate`.
- * Returns `null` if not found.
- */
-export function getPreviousSibling(element: Element, predicate: (sibling: Element) => boolean) {
-    let current: Element = element
-
-    while (current.previousElementSibling) {
-        current = current.previousElementSibling
-
-        if (predicate(current)) {
-            return current
-        }
-    }
-
-    return null
+export function composeHandlers(...handlers: Array<(...params: any) => void>) {
+  return (...args: any[]) => handlers.forEach(handler => handler(...args))
 }
