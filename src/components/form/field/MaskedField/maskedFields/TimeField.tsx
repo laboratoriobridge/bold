bf431c10cd0d1dec6f41d201f54df6f5ea14062f
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-05-15 16:49:22 -0300

    Fix TimeField when setting value to undefined

--- src/components/form/field/MaskedField/maskedFields/TimeField.tsx
+++ src/components/form/field/MaskedField/maskedFields/TimeField.tsx
@@ -3,7 +3,7 @@ import { TimeInput, TimeInputProps } from '../../../input/TimeInput'
 
 export const format = (value: string) => {
   // Omit seconds in case the initial field value is in format 'HH:mm:ss'
-  return value ? value.slice(0, 5) : value
+  return value ? value.slice(0, 5) : null
 }
 
 export type TimeFieldProps = BaseFieldProps<TimeInputProps>
