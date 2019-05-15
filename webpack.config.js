36d3a9a7cd2d712c6a449ab72cc210fa4372bb39
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-05-15 18:33:33 -0300

    Fix uglify minification

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 const path = require('path')
 const { CheckerPlugin } = require('awesome-typescript-loader')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 /**
  * @type {import('webpack').Configuration}
@@ -44,6 +45,22 @@ const config = {
     'react-router-dom',
     'tslint-react',
   ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        uglifyOptions: {
+          warnings: false,
+          compress: {
+            keep_fnames: true,
+          },
+          mangle: {
+            keep_fnames: true,
+          },
+        },
+      }),
+    ],
+  },
 }
 
 module.exports = config
