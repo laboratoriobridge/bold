1136577e7559c8453c92a5aa00da6ac938ffb951
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-06-05 10:20:50 -0300

    Migrate to @next/mdx

--- site/next.config.js
+++ site/next.config.js
@@ -1,5 +1,5 @@
 const withTypescript = require('@zeit/next-typescript')
-const withMDX = require('@zeit/next-mdx')
+const withMDX = require('@next/mdx')
 
 const compose = (...functions) => args => functions.reduceRight((arg, fn) => fn(arg), args)
 
