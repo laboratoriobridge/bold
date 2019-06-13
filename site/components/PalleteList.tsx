d0c931f424b98104463af3e46fe1acb32ddaab15
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-06-13 16:26:53 -0300

    Fix PalleteList order

--- site/components/PalleteList.tsx
+++ site/components/PalleteList.tsx
@@ -4,15 +4,12 @@ import * as colors from '../../lib/styles/colors'
 import { Pallete } from './Pallete'
 
 export const PalleteList = () => {
-  // Put gray first
-  const colorNames = Object.keys(colors).sort((a, b) => (a === 'gray' ? -1 : a > b ? 1 : -1))
-
   return (
     <>
       <Grid justifyContent='flex-start' wrap>
-        {colorNames.map((color: string) => (
-          <Cell key={color} style={{ flex: 0 }}>
-            <Pallete title={capitalize(color)} pallete={(colors as any)[color]} />
+        {Object.entries(colors).map(([name, pallete]) => (
+          <Cell key={name}>
+            <Pallete title={capitalize(name)} pallete={pallete} />
           </Cell>
         ))}
       </Grid>
