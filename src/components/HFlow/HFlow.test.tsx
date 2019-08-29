2834e3c5f8913402214f25753a1272a1de1a10ed
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-08-29 13:45:48 -0300

    Add tests for 'style' prop

--- src/components/HFlow/HFlow.test.tsx
+++ src/components/HFlow/HFlow.test.tsx
@@ -25,3 +25,14 @@ it('should accept some grid props', () => {
   )
   expect(container).toMatchSnapshot()
 })
+
+it('should accept "style" prop', () => {
+  const { container } = render(
+    <HFlow style={{ color: 'red' }}>
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+    </HFlow>
+  )
+  expect(container).toMatchSnapshot()
+})
