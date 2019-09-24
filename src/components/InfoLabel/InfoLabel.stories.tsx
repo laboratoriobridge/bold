4db1f1473db15abf5a684dba6a2e9090db24653d
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-09-24 13:19:36 -0300

    Improve InfoLabel storybook

--- src/components/InfoLabel/InfoLabel.stories.tsx
+++ src/components/InfoLabel/InfoLabel.stories.tsx
@@ -2,13 +2,19 @@ import { text } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 
+import { VFlow } from '../VFlow'
+
 import { InfoLabel } from './InfoLabel'
 
 storiesOf('Components|InfoLabel', module).add('default', () => (
-  <>
-    <InfoLabel title={text('title', 'Nome')} placeholder={text('placeholder', '-')}>
+  <VFlow vSpacing={0.5}>
+    <InfoLabel title={text('title', 'Name')} placeholder={text('placeholder', '-')}>
       João da Silva
     </InfoLabel>
-    <InfoLabel title={text('title', 'Nome')} placeholder={text('placeholder', '-')} />
-  </>
+    <InfoLabel title={text('title', 'Text')} placeholder={text('placeholder', '-')}>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo quasi ullam eos, fuga earum molestiae cum
+      odit, modi exercitationem quis hic quae. Excepturi quia neque expedita explicabo facere corporis dolore?
+    </InfoLabel>
+    <InfoLabel title={text('title', 'Name')} placeholder={text('placeholder', '-')} />
+  </VFlow>
 ))
