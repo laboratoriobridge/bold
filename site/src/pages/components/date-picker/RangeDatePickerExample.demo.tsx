d3a35d62d0ee7f6f8feff89ad6b03c7e057b97d8
Caroline Krone <kronecaroline@gmail.com>
2020-02-07 15:44:14 -0300

    add rangepicker

---
+++ site/src/pages/components/date-picker/RangeDatePickerExample.demo.tsx
@@ -0,0 +1,24 @@
+import { RangeDateField } from 'bold-ui'
+import React, { useState } from 'react'
+
+function RangeDatePickerExample() {
+  const [value, setValue] = useState<Date>()
+
+  const handleChange = (selectedDate: Date) => setValue(selectedDate)
+
+  return (
+    <RangeDateField
+      error=''
+      icon='calendarOutline'
+      label='Period'
+      onChange={function noRefCheck() {}}
+      required
+      value={{
+        finalDate: new Date('2020-02-07T17:46:49.253Z'),
+        startDate: new Date('2020-01-28T17:46:49.253Z'),
+      }}
+    />
+  )
+}
+
+export default RangeDatePickerExample
