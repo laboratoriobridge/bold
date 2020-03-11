f37bdcfd3f3ece3ab9c53f91b00d961a0ce5a054
Felipe Taboada <fliptaboada@gmail.com>
2020-03-11 13:07:08 -0300

    adjusting DatePicker site demos

---
+++ site/src/pages/components/date-picker/DateRangePickerExample.demo.tsx
@@ -0,0 +1,17 @@
+import { DateRangeField, DateRange } from 'bold-ui'
+import React, { useState } from 'react'
+
+function DateRangePickerExample() {
+  const [value, setValue] = useState<DateRange>({
+    startDate: new Date('2020-01-28T17:46:49.253Z'),
+    endDate: new Date('2020-02-07T17:46:49.253Z'),
+  })
+
+  const handleChange = (selectedDate: DateRange) => setValue(selectedDate)
+
+  return (
+    <DateRangeField error='' icon='calendarOutline' label='Period' onChange={handleChange} required value={value} />
+  )
+}
+
+export default DateRangePickerExample
