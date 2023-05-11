a88c9944fb8c4f7f11a2ace797a5947953518ab8
Rafaela Tillmann <rafaelatillmann.rt@gmail.com>
2023-05-11 16:07:50 -0300

    fix alignment between errorText and maxLength on TextArea

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -3,7 +3,7 @@ import React, { CSSProperties } from 'react'
 import { useFormControl, UseFormControlProps } from '../../hooks/useFormControl'
 import { ExternalStyles, Theme, useStyles } from '../../styles'
 import { Omit } from '../../util/types'
-import { FormControl } from '../FormControl'
+import { FormControl, FormError } from '../FormControl'
 import { createStyles as createTextInputBaseStyles } from '../TextField/TextInputBase'
 
 export interface TextAreaProps
@@ -14,15 +14,19 @@ export interface TextAreaProps
 }
 
 export function TextArea(props: TextAreaProps) {
-  const { style, label, error, resize, ...rest } = props
+  const { style, label, resize, ...rest } = props
+  const { error, ...restFormControlProps } = props
 
   const valueLength = props.value && typeof props.value === 'string' && props.value.length
   const defaultValueLength = props.defaultValue && typeof props.defaultValue === 'string' && props.defaultValue.length
   const currentLength = valueLength || defaultValueLength || 0
 
-  const { getFormControlProps, getInputProps } = useFormControl(props)
+  const { getFormControlProps } = useFormControl(restFormControlProps)
+  const { getInputProps } = useFormControl(props)
+
   const inputProps = getInputProps()
   const invalid = inputProps['aria-invalid']
+  const errorId = inputProps['aria-errormessage']
 
   const { classes, css } = useStyles(createStyles, { invalid })
   const className = css(classes.input, invalid && classes.invalid, style, { resize })
@@ -31,19 +35,34 @@ export function TextArea(props: TextAreaProps) {
     <FormControl {...getFormControlProps()}>
       <textarea className={className} {...inputProps} {...rest} />
 
-      {props.maxLength && (
-        <div className={classes.counter}>
-          {currentLength}/{props.maxLength} caracteres
+      <div className={classes.footer}>
+        <div>
+          {error && (
+            <FormError id={errorId} role='alert'>
+              {error}
+            </FormError>
+          )}
         </div>
-      )}
+
+        <div>
+          {props.maxLength && (
+            <div className={classes.counter}>
+              {currentLength}/{props.maxLength} caracteres
+            </div>
+          )}
+        </div>
+      </div>
     </FormControl>
   )
 }
 
 const createStyles = (theme: Theme, { invalid }) => ({
   ...createTextInputBaseStyles(theme),
+  footer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
   counter: {
-    textAlign: 'right',
     color: invalid && theme.pallete.status.danger.main,
   } as CSSProperties,
 })
