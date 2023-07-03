93e97407a52726e143ca0da5d5bd186a562b22ea
wesleydecezere <wesleydecezere@gmail.com>
2023-07-02 22:03:51 -0300

    Add another tests

---
+++ src/components/PivotTable/PivotTableCell/classes/GridArea.test.ts
@@ -0,0 +1,39 @@
+import { GridArea } from './GridArea'
+
+describe('GridArea', () => {
+  it('should initialize it correctly', () => {
+    const input = {
+      rowStart: 0,
+      columnStart: 5,
+      rowEnd: 10,
+      columnEnd: 15,
+    }
+
+    const result = new GridArea(input.rowStart, input.columnStart, input.rowEnd, input.columnEnd)
+
+    expect(result).toEqual(input)
+  })
+
+  it('when dont receive the end values, should set them to their respective start value plus one', () => {
+    const rowStart = 0
+    const columnStart = 5
+
+    const { rowEnd, columnEnd } = new GridArea(rowStart, columnStart)
+
+    expect(rowEnd).toEqual(1)
+    expect(columnEnd).toEqual(6)
+  })
+
+  it('when row end is greater than row start, should throw an error', () => {
+    const rowStart = 5
+    const rowEnd = 0
+
+    expect(() => new GridArea(rowStart, 0, rowEnd, 0)).toThrowError()
+  })
+  it('when column end is greater than clomun start, should throw an error', () => {
+    const columnStart = 5
+    const columnEnd = 0
+
+    expect(() => new GridArea(0, columnStart, 0, columnEnd)).toThrowError()
+  })
+})
