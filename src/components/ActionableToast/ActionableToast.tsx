f6c5d7a3f1c265edee9aaf109ce9fd276f433d5a
Rômulo Fernandes Evangelista <romulo.fernandes@bridge.ufsc.br>
2024-05-16 11:39:36 -0300

    added close button

---
+++ src/components/ActionableToast/ActionableToast.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import { Tooltip } from '../Tooltip'
+import { Button } from '../Button'
+import { useLocale } from '../../i18n'
+import { Icon } from '../Icon'
+import { useStyles } from '../../styles'
+
+export interface ActionableToastProps {
+  message: string
+  onClose?(): any
+  hasCloseIcon?: boolean
+  duration?: number
+}
+
+export function ActionableToast(props: ActionableToastProps) {
+  const { message, duration = 5, hasCloseIcon = true, onClose } = props
+  const [visible, setVisible] = useState(false)
+  const locale = useLocale()
+  const { classes } = useStyles(styles)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false)
+    }, duration)
+
+    return () => clearTimeout(timer)
+  }, [duration])
+
+  return visible ? (
+    <>
+      {hasCloseIcon && (
+        <Tooltip text={locale.modal.close}>
+          <Button
+            aria-label={locale.modal.close}
+            size='small'
+            skin='ghost'
+            style={classes.closeButton}
+            onClick={onClose}
+          >
+            <Icon icon='timesDefault' />
+          </Button>
+        </Tooltip>
+      )}
+      <div className='toast'>{message}</div>{' '}
+    </>
+  ) : null
+}
+
+export const styles = () => ({
+  closeButton: {
+    float: 'right',
+    marginTop: '0.5rem',
+    marginRight: '0.5rem',
+  } as React.CSSProperties,
+})
---
+++ src/components/ActionableToast/index.ts
@@ -0,0 +1,1 @@
+export { ActionableToast } from './ActionableToast'
