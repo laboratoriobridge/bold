704d9d93d5b4c66ea83fc47292747647831fa41c
Rômulo Fernandes Evangelista <romulo.fernandes@bridge.ufsc.br>
2024-05-24 17:36:27 -0300

    wip behavior editing

--- src/hooks/useToastMessages.ts
+++ src/hooks/useToastMessages.ts
@@ -1,31 +1,31 @@
 import { Dispatch, SetStateAction, useState } from 'react'
 import { ActionableToastProps } from '../components/ActionableToast'
-
+export interface ToastMessagesInterface {
+  message: string
+  newToast?: boolean
+  buttonLabel?: string
+  action?: () => void
+  timeoutTimer?: number
+}
 export function useToastMessages(
   erase: boolean = true
 ): {
   toastMessages: ActionableToastProps[]
   setToastMessages: Dispatch<SetStateAction<ActionableToastProps[]>>
-  showToast: (message: string) => void
+  showToast: (variables: ToastMessagesInterface) => void
 } {
   const [toastMessages, setToastMessages] = useState<ActionableToastProps[]>([])
-  function showToast(
-    message: string,
-    newToast: boolean = true,
-    buttonLabel?: string,
-    action?: () => void,
-    timeoutTimer: number = 5
-  ) {
+  function showToast(variables: ToastMessagesInterface) {
     const removeToast = (id: string) => {
       setToastMessages((prevToasts) => prevToasts.filter((toast) => toast.id !== id))
     }
 
     const toast = {
       id: Date.now().toString(),
-      message,
-      action,
-      newToast,
-      buttonLabel,
+      message: variables.message,
+      action: variables.action,
+      newToast: variables.newToast,
+      buttonLabel: variables.buttonLabel,
     }
 
     setToastMessages((prevToasts) => [...prevToasts, toast])
@@ -33,7 +33,7 @@ export function useToastMessages(
     erase &&
       setTimeout(() => {
         removeToast(toast.id)
-      }, timeoutTimer * 1000)
+      }, variables.timeoutTimer * 1000)
   }
 
   return { toastMessages, setToastMessages, showToast }
