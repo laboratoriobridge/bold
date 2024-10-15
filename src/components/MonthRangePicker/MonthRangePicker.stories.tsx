f9e12b1e31886796f4bd1071e856bada64d46b76
zDudaHang <maria.eduarda.melo.hang@gmail.com>
2024-10-15 09:29:49 -0300

    Fixes after internal review

--- src/components/MonthRangePicker/MonthRangePicker.stories.tsx
+++ src/components/MonthRangePicker/MonthRangePicker.stories.tsx
@@ -16,39 +16,39 @@ export default {
   title: 'Components/MonthRangePicker',
 }
 
+const handleChange = (setRange: (referenceMonthRange: ReferenceMonthRange) => void) => (dateRange: DateRange) => {
+  const { startDate, endDate } = dateRange
+
+  if (startDate && endDate) {
+    setRange({
+      start: { month: startDate.getMonth(), year: startDate.getFullYear() },
+      end: { month: endDate.getMonth(), year: endDate.getFullYear() },
+    })
+  } else if (startDate) {
+    setRange({
+      start: { month: startDate.getMonth(), year: startDate.getFullYear() },
+      end: undefined,
+    })
+  } else if (endDate) {
+    setRange({
+      start: undefined,
+      end: { month: endDate.getMonth(), year: endDate.getFullYear() },
+    })
+  } else {
+    setRange({ start: undefined, end: undefined })
+  }
+
+  action('changed')(dateRange)
+}
+
 export const Default = () => {
   const [range, setRange] = useState<ReferenceMonthRange>(initialValue)
-
-  const handleChange = (dateRange: DateRange) => {
-    const { startDate, endDate } = dateRange
-
-    if (startDate && endDate) {
-      setRange({
-        start: { month: startDate.getMonth(), year: startDate.getFullYear() },
-        end: { month: endDate.getMonth(), year: endDate.getFullYear() },
-      })
-    } else if (startDate) {
-      setRange({
-        start: { month: startDate.getMonth(), year: startDate.getFullYear() },
-        end: undefined,
-      })
-    } else if (endDate) {
-      setRange({
-        start: undefined,
-        end: { month: endDate.getMonth(), year: endDate.getFullYear() },
-      })
-    } else {
-      setRange({ start: undefined, end: undefined })
-    }
-
-    action('changed')(dateRange)
-  }
 
   return (
     <MonthRangePicker
       label={text('label', 'Month Field')}
       error={text('error', '')}
-      onChange={handleChange}
+      onChange={(dateRange) => handleChange(setRange)(dateRange)}
       inline={boolean('inline', false)}
       required={boolean('required', false)}
       disabled={boolean('disabled', false)}
@@ -60,36 +60,11 @@ export const Default = () => {
 export const MinMax = () => {
   const [range, setRange] = useState<ReferenceMonthRange>(initialValue)
 
-  const handleChange = (dateRange: DateRange) => {
-    const { startDate, endDate } = dateRange
-
-    if (startDate && endDate) {
-      setRange({
-        start: { month: startDate.getMonth(), year: startDate.getFullYear() },
-        end: { month: endDate.getMonth(), year: endDate.getFullYear() },
-      })
-    } else if (startDate) {
-      setRange({
-        start: { month: startDate.getMonth(), year: startDate.getFullYear() },
-        end: undefined,
-      })
-    } else if (endDate) {
-      setRange({
-        start: undefined,
-        end: { month: endDate.getMonth(), year: endDate.getFullYear() },
-      })
-    } else {
-      setRange({ start: undefined, end: undefined })
-    }
-
-    action('changed')(dateRange)
-  }
-
   return (
     <MonthRangePicker
       label={text('label', 'Month Field')}
       error={text('error', '')}
-      onChange={handleChange}
+      onChange={(dateRange) => handleChange(setRange)(dateRange)}
       disabled={boolean('disabled', false)}
       value={range}
       minMonth={object('minMonth', minMonth)}
