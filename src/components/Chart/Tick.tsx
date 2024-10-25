a75022a8ae4e15464420bed663cab87a13ec9802
AndresKindel <andreskbarbosa1@gmail.com>
2024-10-25 17:33:50 -0300

    feat: update renderAxis to convert tick props
    This commit updates the renderAxis function in the Chart component to convert tick props using the convertTickProps function. This ensures that tick rendering is consistent.

--- src/components/Chart/Tick.tsx
+++ src/components/Chart/Tick.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import { TickProps } from './model'
+import { convertTickProps } from './renderAxis'
 
 export function Tick(props: TickProps) {
   const {
     payload: { value },
   } = props
 
-  return <text {...props}> {props.isOutlierIndicator ? `>${props.domainMaxValue}` : value} </text>
+  return <text {...convertTickProps(props)}> {props.isOutlierIndicator ? `>${props.domainMaxValue}` : value} </text>
 }
