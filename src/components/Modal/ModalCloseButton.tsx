7d8fac21cddfcccb75d9ccd790f88fdb65378334
Jose <jose.daniel@bridge.ufsc.br>
2025-04-16 07:47:27 -0300

    feat(modal): create reusable ModalCloseButton with external style support

---
+++ src/components/Modal/ModalCloseButton.tsx
@@ -0,0 +1,27 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+
+import { useLocale } from '../../i18n'
+import { ExternalStyles } from '../../styles'
+import { Button } from '../Button'
+import { Icon } from '../Icon'
+import { Tooltip } from '../Tooltip'
+
+interface ModalCloseButton {
+  onClose(): any
+  style?: ExternalStyles
+}
+
+export const ModalCloseButton = (props: ModalCloseButton) => {
+  const { onClose, style: externalStyles } = props
+
+  const locale = useLocale()
+
+  return (
+    <Tooltip text={locale.modal.close}>
+      <Button aria-label={locale.modal.close} size='small' skin='ghost' onClick={onClose} style={externalStyles}>
+        <Icon icon='timesDefault' />
+      </Button>
+    </Tooltip>
+  )
+}
