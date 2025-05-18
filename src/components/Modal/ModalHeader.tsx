e5f0a340633398abfabff81a20b635390f6dfcc0
Jose <jose.daniel@bridge.ufsc.br>
2025-05-18 19:54:38 -0300

    refactor(ModalHeader): simplify structure by rendering title and close button directly

--- src/components/Modal/ModalHeader.tsx
+++ src/components/Modal/ModalHeader.tsx
@@ -2,33 +2,45 @@
 import { css, jsx } from '@emotion/core'
 
 import { Color } from 'csstype'
-import { ExternalStyles } from '../../styles'
-import { Omit } from '../../util'
+import { Theme, useTheme } from '../../styles'
+import { Heading } from '../Heading'
+import { ModalCloseButton } from './ModalCloseButton'
 
-export interface ModalHeaderProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'style'> {
-  background?: Color
-  styles?: {
-    wrapper?: ExternalStyles
-    container?: ExternalStyles
-  }
+export type ModalTitleType = string | JSX.Element
+
+export interface ModalHeaderProps {
+  title: ModalTitleType
+  backgroundColor?: Color
+  hasCloseIcon?: boolean
+  onClose: () => void
 }
 
 export const ModalHeader = (props: ModalHeaderProps) => {
-  const { styles: externalStyles, children, ...rest } = props
+  const { title, backgroundColor, hasCloseIcon, onClose } = props
+
+  const theme = useTheme()
+  const styles = createStyles(theme, backgroundColor)
 
   return (
-    <div css={[externalStyles?.container, styles.header]} {...rest}>
-      {children}
+    <div css={styles.header}>
+      {typeof title === 'string' ? <Heading level={1}>{title}</Heading> : title}
+      {hasCloseIcon && <ModalCloseButton onClose={onClose} />}
     </div>
   )
 }
 
-const styles = {
+const createStyles = (theme: Theme, backgroundColor: Color) => ({
   header: css`
-    flex: 1;
-    min-width: 0;
-    word-break: break-word;
-    white-space: normal;
-    overflow: hidden;
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    height: fit-content;
+    padding: 1.5rem 1rem 1.5rem 2rem;
+    gap: 1rem;
+    align-items: center;
+    background-color: ${backgroundColor || theme.pallete.surface.background};
+    box-shadow: 0 1px 5px 0 ${theme.pallete.divider}, 0 2px 1px -1px ${theme.pallete.divider};
+    z-index: 1;
   `,
-}
+})
