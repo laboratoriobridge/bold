3701994d159919989dd79b62fc03c556c5eec251
Jose <jose.daniel@bridge.ufsc.br>
2025-05-19 18:09:41 -0300

    feat(ModalHeader): add support for subtitle, icon and their visual styles, and remove JSX from title prop

--- src/components/Modal/ModalHeader.tsx
+++ src/components/Modal/ModalHeader.tsx
@@ -4,43 +4,76 @@ import { css, jsx } from '@emotion/core'
 import { Color } from 'csstype'
 import { Theme, useTheme } from '../../styles'
 import { Heading } from '../Heading'
+import { IconImage } from '../Icon'
+import { Icon, IconColor } from '../Icon/Icon'
+import { HFlow } from '../HFlow'
+import { VFlow } from '../VFlow'
 import { ModalCloseButton } from './ModalCloseButton'
 
-export type ModalTitleType = string | JSX.Element
+export type SurfaceColor = 'main' | 'background'
 
 export interface ModalHeaderProps {
-  title: ModalTitleType
-  backgroundColor?: Color
+  title: string
+  subtitle?: string
+  icon?: IconImage
+  iconFill?: IconColor
+  iconStroke?: IconColor
   hasCloseIcon?: boolean
+  backgroundColor?: SurfaceColor
+  hasDivider?: boolean
   onCloseButtonClick: () => void
 }
 
 export const ModalHeader = (props: ModalHeaderProps) => {
-  const { title, backgroundColor, hasCloseIcon, onCloseButtonClick } = props
+  const {
+    title,
+    subtitle,
+    icon,
+    iconFill,
+    iconStroke,
+    backgroundColor = 'main',
+    hasDivider = true,
+    hasCloseIcon,
+    onCloseButtonClick,
+  } = props
 
   const theme = useTheme()
-  const styles = createStyles(theme, backgroundColor)
+  const styles = createStyles(theme, backgroundColor, hasDivider)
 
   return (
-    <div css={styles.header} data-testid='modal-header'>
-      {typeof title === 'string' ? <Heading level={1}>{title}</Heading> : title}
+    <HFlow
+      hSpacing={1}
+      justifyContent='space-between'
+      alignItems='flex-start'
+      data-testid='modal-header'
+      style={styles.header}
+    >
+      <HFlow hSpacing={1.25} justifyContent='flex-start' alignItems='flex-start'>
+        {icon && <Icon icon={icon} size={3} fill={iconFill} stroke={iconStroke} />}
+        <VFlow vSpacing={0}>
+          <Heading level={1} color='normal' fontWeight='bold'>
+            {title}
+          </Heading>
+          {subtitle && (
+            <Heading level={5} color='normal' fontWeight='normal'>
+              {subtitle}
+            </Heading>
+          )}
+        </VFlow>
+      </HFlow>
       {hasCloseIcon && <ModalCloseButton onClick={onCloseButtonClick} />}
-    </div>
+    </HFlow>
   )
 }
 
-const createStyles = (theme: Theme, backgroundColor: Color) => ({
+const createStyles = (theme: Theme, backgroundColor: Color, hasDivider: Boolean) => ({
   header: css`
     position: relative;
-    display: flex;
-    justify-content: space-between;
+    z-index: 1;
     width: 100%;
     height: fit-content;
-    padding: 1.5rem 1rem 1.5rem 2rem;
-    gap: 1rem;
-    align-items: center;
-    background-color: ${backgroundColor || theme.pallete.surface.background};
-    box-shadow: 0 1px 5px 0 ${theme.pallete.divider}, 0 2px 1px -1px ${theme.pallete.divider};
-    z-index: 1;
+    padding: 1.5rem 1rem 1rem 2rem;
+    background-color: ${theme.pallete.surface[backgroundColor]};
+    ${hasDivider && `box-shadow: 0 1px 5px 0 ${theme.pallete.divider}, 0 2px 1px -1px ${theme.pallete.divider};`}
   `,
 })
