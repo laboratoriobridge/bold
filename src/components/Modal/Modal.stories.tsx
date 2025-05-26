c4c3e90fcadbe49209bca53cf4dd7a98b13d564c
Jose <jose.daniel@bridge.ufsc.br>
2025-05-26 18:58:10 -0300

    feat(storybook): add header prop to Modal and knobs for style customization

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,8 +1,12 @@
 import { action } from '@storybook/addon-actions'
-import { boolean, select } from '@storybook/addon-knobs'
+import { boolean, select, text } from '@storybook/addon-knobs'
 import React from 'react'
+import { TextColor } from '../../styles'
+import { textColorMap } from '../../styles/theme/createPallete'
 import { Button } from '../Button'
 import { HFlow } from '../HFlow'
+import { Icons } from '../Icon'
+import { IconMap } from '../Icon/generated/types'
 import { modal } from './auto'
 import { ModalMountTarget } from './auto/ModalMountTarget'
 import { Modal, ModalSize } from './Modal'
@@ -16,49 +20,71 @@ const sizes: { [key in ModalSize]: ModalSize } = {
   auto: 'auto',
 }
 
+const icons: Icons[] = Object.keys(IconMap) as Icons[]
+const iconColors: TextColor[] = Object.keys(textColorMap) as TextColor[]
+
 export default {
   title: 'Components/Modal',
 }
 
-export const Default = () => (
-  <>
-    <p>Default</p>
+export const Default = () => {
+  const open = boolean('open', true)
+  const size = select('size', sizes, 'large')
+  const title = text('title', 'Modal Title')
+  const subtitle = title ? text('subtitle', 'Modal Subtitle') : undefined
+  const header = title
+    ? {
+        icon: {
+          name: select('header.icon.name', icons, 'bridge'),
+          fill: select('header.icon.fill', iconColors, 'primary'),
+          stroke: select('header.icon.stroke', iconColors, 'inherit'),
+        },
+        background: text('header.icon.background', 'white'),
+        showBottomBorder: boolean('header.icon.showBottomBorder', true),
+      }
+    : undefined
+  const onClose = action('close')
 
-    <Modal open={boolean('open', true)} size={select('size', sizes, 'large')} onClose={action('close')}>
-      <ModalBody>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, dui id luctus commodo, nunc enim
-          facilisis odio, vel hendrerit erat neque eu nisl. Donec urna felis, pharetra nec urna a, efficitur lobortis
-          urna. Mauris varius purus vehicula lorem mollis, a cursus enim malesuada. Integer at congue enim. Nullam purus
-          mauris, fermentum nec mattis in, cursus nec tellus. Nunc sodales orci tortor, at feugiat purus hendrerit a.
-          Suspendisse potenti. Nam porta urna vitae nibh pharetra eleifend. Nullam urna eros, auctor vitae maximus non,
-          feugiat eget odio. Cras venenatis, lectus eget consectetur volutpat, urna felis efficitur enim, vitae viverra
-          purus risus sed purus.
-        </p>
+  return (
+    <div>
+      <p>Default</p>
 
-        <p>
-          Aliquam placerat eget massa vel congue. Suspendisse libero erat, auctor ac tristique et, ultrices quis dolor.
-          Suspendisse commodo lacinia lectus ut egestas. Cras in urna eget leo mollis luctus eu eget felis. Mauris vel
-          libero vel lectus cursus placerat. Nunc non ligula urna. Aliquam id quam at quam sodales gravida condimentum
-          quis eros. am dolor nisl, ullamcorper ac varius nec, vulputate sed metus. Cras lacus ante, dapibus facilisis
-          risus at, varius commodo nulla. Nunc volutpat pellentesque mauris et sodales. Suspendisse finibus, dui at
-          tristique mollis, felis mauris vulputate leo, sed vehicula lacus nibh ut dolor.
-        </p>
-      </ModalBody>
-      <ModalFooter>
-        <HFlow justifyContent='flex-end'>
-          <Button onClick={action('cancel clicked')}>Cancel</Button>
-          <Button kind='primary' onClick={action('save clicked')}>
-            Save
-          </Button>
-        </HFlow>
-      </ModalFooter>
-    </Modal>
-  </>
-)
+      <Modal open={open} size={size} title={title} subtitle={subtitle} header={header} onClose={onClose}>
+        <ModalBody>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, dui id luctus commodo, nunc enim
+            facilisis odio, vel hendrerit erat neque eu nisl. Donec urna felis, pharetra nec urna a, efficitur lobortis
+            urna. Mauris varius purus vehicula lorem mollis, a cursus enim malesuada. Integer at congue enim. Nullam
+            purus mauris, fermentum nec mattis in, cursus nec tellus. Nunc sodales orci tortor, at feugiat purus
+            hendrerit a. Suspendisse potenti. Nam porta urna vitae nibh pharetra eleifend. Nullam urna eros, auctor
+            vitae maximus non, feugiat eget odio. Cras venenatis, lectus eget consectetur volutpat, urna felis efficitur
+            enim, vitae viverra purus risus sed purus.
+          </p>
+
+          <p>
+            Aliquam placerat eget massa vel congue. Suspendisse libero erat, auctor ac tristique et, ultrices quis
+            dolor. Suspendisse commodo lacinia lectus ut egestas. Cras in urna eget leo mollis luctus eu eget felis.
+            Mauris vel libero vel lectus cursus placerat. Nunc non ligula urna. Aliquam id quam at quam sodales gravida
+            condimentum quis eros. am dolor nisl, ullamcorper ac varius nec, vulputate sed metus. Cras lacus ante,
+            dapibus facilisis risus at, varius commodo nulla. Nunc volutpat pellentesque mauris et sodales. Suspendisse
+            finibus, dui at tristique mollis, felis mauris vulputate leo, sed vehicula lacus nibh ut dolor.
+          </p>
+        </ModalBody>
+        <ModalFooter>
+          <HFlow justifyContent='flex-end'>
+            <Button onClick={action('cancel clicked')}>Cancel</Button>
+            <Button kind='primary' onClick={action('save clicked')}>
+              Save
+            </Button>
+          </HFlow>
+        </ModalFooter>
+      </Modal>
+    </div>
+  )
+}
 
 export const Auto = () => (
-  <>
+  <div>
     <Button
       onClick={modal({
         size: 'small',
@@ -72,7 +98,7 @@ export const Auto = () => (
       Auto modal
     </Button>
     <ModalMountTarget />
-  </>
+  </div>
 )
 
 export const Parts = () => (
@@ -88,10 +114,10 @@ export const Parts = () => (
 )
 
 export const ModalOverlap = () => (
-  <>
+  <div>
     <p>ModalOverlap</p>
 
-    <Modal open={true} size={select('main modal size', sizes, 'large')} onClose={action('close')}>
+    <Modal open={boolean('open', true)} size={select('main modal size', sizes, 'large')} onClose={action('close')}>
       <ModalBody>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, dui id luctus commodo, nunc enim
@@ -126,5 +152,5 @@ export const ModalOverlap = () => (
       </ModalFooter>
     </Modal>
     <ModalMountTarget />
-  </>
+  </div>
 )
