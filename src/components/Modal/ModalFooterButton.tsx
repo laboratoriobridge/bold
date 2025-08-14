72a964b9a5b05892e816fc1ca0848b01cc89ecee
Diego Mello <diegoasmello@gmail.com>
2025-08-14 11:32:18 -0300

    declare styles outside the component

--- src/components/Modal/ModalFooterButton.tsx
+++ src/components/Modal/ModalFooterButton.tsx
@@ -7,11 +7,13 @@ export interface ModalFooterButtonProps extends Omit<ButtonProps, 'size'> {}
 export function ModalFooterButton(props: ModalFooterButtonProps) {
   const { style, ...rest } = props
 
-  const { classes } = useStyles(() => ({
-    button: {
-      minWidth: '9rem',
-    },
-  }))
+  const { classes } = useStyles(createStyles)
 
   return <Button style={[classes.button, style]} {...rest} />
 }
+
+const createStyles = () => ({
+  button: {
+    minWidth: '9rem',
+  },
+})
