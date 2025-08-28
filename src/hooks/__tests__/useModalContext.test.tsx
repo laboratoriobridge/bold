cef7c87ca490247c8747fbd85522444e4558cd42
Diego Mello <diegoasmello@gmail.com>
2025-08-28 09:46:13 -0300

    modal context tests

--- src/hooks/__tests__/useModalContext.test.tsx
+++ src/hooks/__tests__/useModalContext.test.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { useModalContext, ModalContextProvider, ModalContextValue } from '../useModalContext'
+import { ModalBody, ModalHeader } from '../../components/Modal'
 
 function TestComponent() {
-  const { scroll, bodyRef } = useModalContext()
+  const { scroll, bodyRef, hasHeader } = useModalContext()
 
   return (
     <div>
       <span data-testid='modal-context-scroll-value'>{scroll}</span>
       <span data-testid='modal-context-body-ref'>{bodyRef ? 'has-ref' : 'no-ref'}</span>
+      <span data-testid='modal-context-has-header'>{hasHeader ? 'has-header' : 'no-header'}</span>
     </div>
   )
 }
@@ -17,7 +19,7 @@ it('should return provided context values when inside ModalContextProvider', () 
   const mockContextValue: ModalContextValue = {
     bodyRef: { current: document.createElement('div') },
     scroll: 'body',
-    hasHeader: false,
+    hasHeader: true,
     setHasHeader: jest.fn(),
   }
 
@@ -29,6 +31,7 @@ it('should return provided context values when inside ModalContextProvider', () 
 
   expect(screen.getByTestId('modal-context-scroll-value').textContent).toBe('body')
   expect(screen.getByTestId('modal-context-body-ref').textContent).toBe('has-ref')
+  expect(screen.getByTestId('modal-context-has-header').textContent).toBe('has-header')
 })
 
 it('should throw an error when used outside ModalContextProvider', () => {
@@ -37,4 +40,42 @@ it('should throw an error when used outside ModalContextProvider', () => {
   expect(renderWithoutProvider).toThrow(
     'Modal subcomponents (ModalHeader, ModalBody, ModalContainer, ModalFooter, ModalCloseButton) must be used inside <Modal>'
   )
+})
+
+it('should call setHasHeader when modal has header', () => {
+  const mockSetHasHeader = jest.fn()
+
+  const mockContextValue: ModalContextValue = {
+    bodyRef: { current: document.createElement('div') },
+    scroll: 'body',
+    hasHeader: true,
+    setHasHeader: mockSetHasHeader,
+  }
+
+  render(
+    <ModalContextProvider value={mockContextValue}>
+      <ModalHeader title='Modal title' />
+    </ModalContextProvider>
+  )
+
+  expect(mockSetHasHeader).toHaveBeenCalledTimes(1)
+})
+
+it('should not call setHasHeader when modal has header', () => {
+  const mockSetHasHeader = jest.fn()
+
+  const mockContextValue: ModalContextValue = {
+    bodyRef: { current: document.createElement('div') },
+    scroll: 'body',
+    hasHeader: false,
+    setHasHeader: mockSetHasHeader,
+  }
+
+  render(
+    <ModalContextProvider value={mockContextValue}>
+      <ModalBody>Modal body content</ModalBody>
+    </ModalContextProvider>
+  )
+
+  expect(mockSetHasHeader).not.toHaveBeenCalled()
 })
