77de9ba3255eaa2b76d71052565a1e8a62206352
Diego Mello <diegoasmello@gmail.com>
2025-08-28 16:46:05 -0300

    wip: modal sidebar

---
+++ src/components/Modal/ModalSidebar.test.tsx
@@ -0,0 +1,63 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import { ModalContextValue } from '../../hooks'
+import { ModalContextProvider } from '../../hooks/useModalContext'
+import { ModalScroll } from './Modal'
+import { ModalSidebar } from './ModalSidebar'
+
+const mockContextValue: ModalContextValue = {
+  bodyRef: { current: document.createElement('div') },
+  scroll: 'body' as ModalScroll,
+  hasHeader: false,
+  hasLeftSidebar: true,
+  hasRightSidebar: false,
+  setHasHeader: jest.fn(),
+  setHasLeftSidebar: jest.fn(),
+  setHasRightSidebar: jest.fn(),
+}
+
+it('should render correctly', () => {
+  const { container } = render(
+    <ModalContextProvider value={mockContextValue}>
+      <ModalSidebar side='left'>Content</ModalSidebar>
+    </ModalContextProvider>
+  )
+  expect(container).toMatchSnapshot()
+})
+
+it('should accept "style" prop', () => {
+  const { container } = render(
+    <ModalContextProvider value={mockContextValue}>
+      <ModalSidebar side='left' style={{ background: 'red' }}>
+        Content
+      </ModalSidebar>
+    </ModalContextProvider>
+  )
+  expect(container).toMatchSnapshot()
+})
+
+it('should apply border right when side is left', () => {
+  const { getByTestId } = render(
+    <ModalContextProvider value={mockContextValue}>
+      <ModalSidebar side='left' data-testid='sidebar-left-border'>
+        Content
+      </ModalSidebar>
+    </ModalContextProvider>
+  )
+  const sidebar = getByTestId('sidebar-left-border')
+  expect(getComputedStyle(sidebar).borderRight).toBe('1px solid #D3D4DD')
+  expect(getComputedStyle(sidebar).borderLeft).toBe('0px')
+})
+
+it('should apply border left when side is right', () => {
+  const { getByTestId } = render(
+    <ModalContextProvider value={mockContextValue}>
+      <ModalSidebar side='right' data-testid='sidebar-right-border'>
+        Content
+      </ModalSidebar>
+    </ModalContextProvider>
+  )
+  const sidebar = getByTestId('sidebar-right-border')
+  expect(getComputedStyle(sidebar).borderLeft).toBe('1px solid #D3D4DD')
+  expect(getComputedStyle(sidebar).borderRight).toBe('0px')
+})
---
+++ src/components/Modal/ModalSidebar.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react'
+import { ExternalStyles, Theme, useStyles } from '../../styles'
+import { useModalContext } from '../../hooks'
+import { ModalScroll } from './Modal'
+
+export type ModalSidebarType = 'left' | 'right'
+
+export interface ModalSidebarProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'style'> {
+  side: ModalSidebarType
+  style?: ExternalStyles
+}
+
+export function ModalSidebar(props: ModalSidebarProps) {
+  const { side, style, children, ...rest } = props
+
+  const { scroll, setHasLeftSidebar, setHasRightSidebar } = useModalContext()
+  const { classes } = useStyles(createStyles, side, scroll)
+
+  useEffect(() => {
+    if (side === 'left') {
+      setHasLeftSidebar(true)
+      return () => setHasLeftSidebar(false)
+    }
+    if (side === 'right') {
+      setHasRightSidebar(true)
+      return () => setHasRightSidebar(false)
+    }
+  }, [setHasLeftSidebar, setHasRightSidebar, side])
+
+  return (
+    <div className={classes.sidebar} {...rest}>
+      {children}
+    </div>
+  )
+}
+
+const createStyles = (theme: Theme, side: ModalSidebarType, scroll: ModalScroll) => ({
+  sidebar: {
+    background: theme.pallete.gray.c90,
+    border: 0,
+    borderLeft: side === 'right' && `1px solid ${theme.pallete.divider}`,
+    borderRight: side === 'left' && `1px solid ${theme.pallete.divider}`,
+    padding: '1rem',
+    minWidth: '16.875rem',
+    maxWidth: '20rem',
+    overflowY: scroll === 'body' ? 'auto' : 'initial',
+    gridRow: '2',
+    gridColumn: side === 'left' ? '1' : '3',
+  } as React.CSSProperties,
+})
