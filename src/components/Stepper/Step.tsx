af25541625c830ee92f9a1b86dde439d8444adf4
Diego Mello <diegoasmello@gmail.com>
2025-09-26 16:57:44 -0300

    refactor: move icon logic to StepIcon component

--- src/components/Stepper/Step.tsx
+++ src/components/Stepper/Step.tsx
@@ -1,10 +1,8 @@
-import React, { CSSProperties, ElementType, ReactNode, useEffect, useRef } from 'react'
+import React, { CSSProperties, ElementType, HTMLAttributes, ReactNode, useEffect, useRef } from 'react'
 
-import { ExternalStyles, focusBoxShadow, Theme, useStyles } from '../../styles'
-import { isNil, Omit } from '../../util'
+import { ExternalStyles, useStyles } from '../../styles'
 import { getComponents } from '../../util/overrides'
-import CheckIcon from '../Icon/generated/CheckDefault'
-import MinusCircleFilled from '../Icon/generated/MinusCircleFilled'
+
 import { VFlow } from '../VFlow'
 import { useHeight } from '../../hooks/useMeasure'
 import { useStepperContext } from './useStepperContext'
@@ -12,33 +10,32 @@ import { StepperDirection } from './Stepper'
 import { StepConnector } from './StepConnector'
 import { StepContent } from './StepContent'
 import { StepLabel } from './StepLabel'
+import { StepIcon } from './StepIcon'
 
 export type StepStatus = 'active' | 'completed' | 'incompleted' | 'inactive'
 
-export interface StepProps extends Omit<React.HTMLAttributes<HTMLSpanElement>, 'style' | 'title'> {
+export interface StepProps extends Omit<HTMLAttributes<HTMLSpanElement>, 'style' | 'title'> {
   status?: StepStatus
   title: ReactNode
   subtitle?: ReactNode
   style?: ExternalStyles
   overrides?: {
-    Root?: React.ElementType
-    IconContainer?: React.ElementType
-    Icon?: React.ElementType
+    Root?: ElementType
+    Icon?: ElementType
   }
 }
 
 export function Step(props: StepProps) {
   const { status = 'incompleted', title, subtitle, overrides, children, style, ...rest } = props
-  const { Root, Icon, IconContainer } = getComponents(overrides, defaultComponents)
+  const { Root } = getComponents(overrides, defaultComponents)
 
   const { direction, getNextStepStatus, registerStep } = useStepperContext()
-  const { classes, css } = useStyles((theme) => createStyles(theme, status, direction))
+  const { classes, css } = useStyles(() => createStyles(direction))
 
   const [labelRef, labelHeight] = useHeight()
   const stepIndexRef = useRef<number | null>(null)
 
   const stepIndex = stepIndexRef.current ?? 0
-  const hasBackground = getHasBackground(status, overrides?.Icon)
   const nextStepStatus = getNextStepStatus(stepIndex)
   const isLastStep = nextStepStatus === undefined
 
@@ -54,13 +51,7 @@ export function Step(props: StepProps) {
 
       <VFlow vSpacing={0}>
         <div className={classes.labelContainer} ref={labelRef}>
-          <IconContainer className={classes.iconContainer}>
-            {hasBackground && <div className={classes.background} />}
-            {Icon && <Icon className={classes.icon} />}
-            {!Icon && status === 'completed' && <CheckIcon className={classes.icon} />}
-            {!Icon && status === 'inactive' && <MinusCircleFilled className={classes.icon} />}
-          </IconContainer>
-
+          <StepIcon status={status} overrides={overrides} />
           <StepLabel status={status} title={title} subtitle={subtitle} />
         </div>
 
@@ -73,10 +64,9 @@ export function Step(props: StepProps) {
 export const defaultComponents: StepProps['overrides'] = {
   Root: 'span',
   Icon: null,
-  IconContainer: 'span',
 }
 
-const createStyles = (theme: Theme, status: StepStatus, direction: StepperDirection) => {
+const createStyles = (direction: StepperDirection) => {
   const isHorizontal = direction === 'horizontal'
 
   return {
@@ -90,38 +80,5 @@ const createStyles = (theme: Theme, status: StepStatus, direction: StepperDirect
       alignItems: 'center',
       gap: isHorizontal ? '0.75rem' : '0.5rem',
     } as CSSProperties,
-    iconContainer: {
-      zIndex: 1,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '1.5rem',
-      height: '1.5rem',
-      textAlign: 'center',
-      position: 'relative',
-    } as CSSProperties,
-    background: {
-      width: '1rem',
-      height: '1rem',
-      borderRadius: '50%',
-      position: 'absolute',
-      top: 'calc(50% - 0.5rem)',
-      left: 'calc(50% - 0.5rem)',
-      background: status === 'incompleted' ? theme.pallete.gray.c60 : theme.pallete.primary.main,
-      boxShadow:
-        (status === 'active' && focusBoxShadow(theme, 'primary')) ||
-        (status === 'completed' && `0 0 0 4px ${theme.pallete.primary.main}`),
-      transition: 'all .4s ease',
-    } as CSSProperties,
-    icon: {
-      zIndex: 1,
-      fill: status === 'completed' || status === 'active' ? theme.pallete.primary.c100 : theme.pallete.gray.c60,
-      width: '1.25rem',
-      height: '1.25rem',
-    } as CSSProperties,
   }
-}
-
-const getHasBackground = (status: StepStatus, overridedIcon: ElementType) => {
-  return status === 'active' || status === 'completed' || (status === 'incompleted' && isNil(overridedIcon))
 }
---
+++ src/components/Stepper/StepIcon.tsx
@@ -0,0 +1,67 @@
+import React, { CSSProperties, ElementType } from 'react'
+import { ExternalStyles, focusBoxShadow, Theme, useStyles } from '../../styles'
+import CheckIcon from '../Icon/generated/CheckDefault'
+import MinusCircleFilled from '../Icon/generated/MinusCircleFilled'
+import { isNil } from '../../util'
+import { StepStatus } from './Step'
+
+interface StepIconProps extends Omit<React.HTMLAttributes<HTMLSpanElement>, 'style'> {
+  status: StepStatus
+  overrides?: {
+    Icon?: ElementType
+  }
+  style?: ExternalStyles
+}
+
+export function StepIcon(props: StepIconProps) {
+  const { status, overrides: { Icon } = {}, style, ...rest } = props
+
+  const { classes, css } = useStyles((theme) => createStyles(theme, status))
+
+  const hasBackground = getHasBackground(status, Icon)
+
+  return (
+    <span className={css(classes.iconContainer, style)} {...rest}>
+      {hasBackground && <div className={classes.background} />}
+      {Icon && <Icon className={classes.icon} />}
+      {!Icon && status === 'completed' && <CheckIcon className={classes.icon} />}
+      {!Icon && status === 'inactive' && <MinusCircleFilled className={classes.icon} />}
+    </span>
+  )
+}
+
+const createStyles = (theme: Theme, status: StepStatus) => ({
+  iconContainer: {
+    zIndex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '1.5rem',
+    height: '1.5rem',
+    textAlign: 'center',
+    position: 'relative',
+  } as CSSProperties,
+  background: {
+    width: '1rem',
+    height: '1rem',
+    borderRadius: '50%',
+    position: 'absolute',
+    top: 'calc(50% - 0.5rem)',
+    left: 'calc(50% - 0.5rem)',
+    background: status === 'incompleted' ? theme.pallete.gray.c60 : theme.pallete.primary.main,
+    boxShadow:
+      (status === 'active' && focusBoxShadow(theme, 'primary')) ||
+      (status === 'completed' && `0 0 0 4px ${theme.pallete.primary.main}`),
+    transition: 'all .4s ease',
+  } as CSSProperties,
+  icon: {
+    zIndex: 1,
+    fill: status === 'completed' || status === 'active' ? theme.pallete.primary.c100 : theme.pallete.gray.c60,
+    width: '1.25rem',
+    height: '1.25rem',
+  } as CSSProperties,
+})
+
+const getHasBackground = (status: StepStatus, overridedIcon: ElementType) => {
+  return status === 'active' || status === 'completed' || (status === 'incompleted' && isNil(overridedIcon))
+}
