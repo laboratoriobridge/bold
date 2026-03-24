0f8e6f89832eea854345f82a6b19556466acc4fd
AndresKindel <andreskbarbosa1@gmail.com>
2026-03-24 16:55:07 -0300

    add new icons

---
+++ src/components/Icon/generated/CalendarWarningFilled.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SvgCalendarWarningFilled = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width='1em' height='1em' viewBox='0 0 24 24' {...props} />
+)
+
+export default SvgCalendarWarningFilled
